6119c68d4d139371d04688e8b1d55adc634670d8
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-22 13:38:39 -0700

    add-to-cart-function

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,8 +2,12 @@ import { useRouter } from "next/router"
 import Layout from "../../components/Layout"
 import productItems from "../../data/products.json"
 import Image from "next/image"
+import { useContext } from "react"
+import { CartContext } from '../../context/Cart'
 
 function ProductPage() {
+  
+  const {state, dispatch } = useContext(CartContext)
   const { query } = useRouter()
   const { slug } = query
 
@@ -11,6 +15,21 @@ function ProductPage() {
 
   if (!product) {
     return <div>Product not found.</div>
+  }
+
+  function addToCartHandler() {
+    const existingItem = state.cart.cartItems.find((item) => item.slug === product.slug)
+    const quantity = existingItem ? existingItem.quantity + 1 : 1
+
+    if (product.count < quantity) {
+      alert("Sorry. Product is out of stock")
+      return
+    }
+
+    dispatch({
+      type: "ADD_ITEM",
+      payload: { ...product, quantity },
+    })
   }
 
   return (
@@ -43,7 +62,7 @@ function ProductPage() {
             <div>Status:</div>
             <div>{product.count > 0 ? "Available" : "Unavailable"}</div>
           </div>
-          <button className="rounded-xl bg-gray-700 text-white px-4 py-2 w-full">
+          <button onClick={addToCartHandler} className="rounded-xl bg-gray-700 text-white px-4 py-2 w-full">
             Add to Cart
           </button>
         </div>
