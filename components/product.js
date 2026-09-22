3eb537e57e672f28a703f29c3daa9933c68512be
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-22 10:30:09 -0700

    create-product-page

--- components/product.js
+++ components/product.js
@@ -20,7 +20,7 @@ function Product({ item }) {
           <h2 className="text-lg">{item.title}</h2>
         </Link>
 
-        <p className="p-2">{item.price}</p>
+        <p className="p-2">${item.price}</p>
 
         <button className="rounded-xl bg-gray-700 text-white px-4 py-2">
           Add to Cart
