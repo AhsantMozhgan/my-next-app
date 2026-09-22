7fd2b9a726c6dfc787358cd9da010d03054f83ec
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-22 10:11:09 -0700

    show-products

--- components/product.js
+++ components/product.js
@@ -1,24 +1,33 @@
 import Link from "next/link"
+import Image from "next/image"
 
-function Product({item}) {
-    return(
-        <div className="bg-white rounded-xl mb-5 block">
-            <Link href={`/product/${item.slug}`}>
-            <a>
-                <img src="{item.image} className="rounded-t-xl/>
-            </a>
-            </Link>
-            <div className="flex flex-col items-center justify-center p-5">
-                <Link href={`/product/${item.slug}`}>
-                <a>
-                    <h2 className="text-lg">{item.title}</h2>
-                </a>
-                </Link>
-                <p className="p-2">{item.price}</p>
-                <button className="rounded-xl bg-gray-700 text-white px-4 py-2">Add to Cart</button>
-            </div>
-        </div>
-    )
+function Product({ item }) {
+  return (
+    <div className="bg-white rounded-xl mb-5 block">
+        
+        <Link href={`/product/${item.slug}`}>
+        <Image
+            src={item.image}
+            alt={item.title}
+            width={400}
+            height={400}
+            className="rounded-t-xl"
+        />
+        </Link>
+
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${item.slug}`}>
+          <h2 className="text-lg">{item.title}</h2>
+        </Link>
+
+        <p className="p-2">{item.price}</p>
+
+        <button className="rounded-xl bg-gray-700 text-white px-4 py-2">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Product
