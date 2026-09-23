df74693daf6ca9c88586a590ed976e849b2c841c
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-23 14:08:50 -0700

    install-bcryptjs

---
+++ data/users.js
@@ -0,0 +1,18 @@
+import bcrypt from "bcryptjs"
+
+const users = [
+  {
+    name: "Mozhgan",
+    email: "admin@example.com",
+    password: bcrypt.hashSync('123456'),
+    isAdmin: true,
+  },
+  {
+    name: "User Two",
+    email: "user@example.com",
+    password: bcrypt.hashSync('123456'),
+    isAdmin: false,
+  },
+];
+
+export default users
